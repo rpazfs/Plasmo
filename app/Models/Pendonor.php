644b7bc96fb330f8f9a351d1dba281bafd208b90
figoperdana <figoperdana@users.noreply.github.com>
2021-12-12 11:44:18 +0700

    update

---
+++ app/Models/Pendonor.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pendonor extends Model
+{
+    use HasFactory;
+}
