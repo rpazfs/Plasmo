a76890dd6a2f2906888a2ca11bc590b0e9736189
figoperdana <figoperdana@users.noreply.github.com>
2021-12-15 00:01:07 +0700

    update database

--- database/migrations/2021_12_10_180058_create_pendonor_table.php
+++ database/migrations/2021_12_10_180058_create_pendonor_table.php
@@ -15,15 +15,27 @@ class CreatePendonorTable extends Migration
     {
         Schema::create('pendonor', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('username', 255)->unique();
+            $table->string('name', 255);
             $table->string('hotline');
             $table->string('gender');
-            $table->string('blood_type');
-            $table->string('NIK');
+            $table->string('rhesusblood_type');
+            $table->string('NIK', 255);
             $table->integer('age');
             $table->integer('weight');
+            $table->integer('height');
             $table->string('covid_infected');
-            $table->string('form_pendonor');
+            $table->string('province');
+            $table->string('city');
+            $table->string('districts');
+            $table->string('street');
+            $table->string('donors');
+            $table->string('donors_apheresis');
+            $table->string('donors_hospital');
+            $table->string('PCR_Positive');
+            $table->string('PCR_Negative');
+            $table->string('PCR_Positive_file')->nullable();
+            $table->string('PCR_Negative_file')->nullable();
             $table->timestamps();
         });
     }
